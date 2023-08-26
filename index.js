ca26c1384f15d779582dbc0ba67c5ac7d44a49b8
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2023-08-26 22:56:33 +0300

    fixed

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
+require('dotenv').config();
 const express = require('express');
 const { render } = require('express/lib/response');
 const {google} = require('googleapis');
 const path = require('path');
-const GOOGLE_APPLICATION_CREDENTIALS = './credentials.json';
+const {JWT} = require('google-auth-library');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+
+
+
+
+
 const app = express();
 const spreadsheetId = '1Pua7Qa8pTm-5l2GXHHJVoOpkS2ny7u-AIGSzxTU1yTA';
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',async (req, res) => {
+    //create a google autn client for service account
+    const auth = new JWT({
+        email: process.env.CLIENT_EMAIL,
+        key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
     res.render('index');
+
+
+    /* const doc = new GoogleSpreadsheet(spreadsheetId, auth);
+    await doc.loadInfo();
+    console.log(doc.sheetsByIndex[0]); */
+    
+
 });
 app.post('/',async (req, res) => {
     const {name,phone} = req.body;
     //create a google autn client for service account   
-    const auth = new google.auth.GoogleAuth({
-        keyFile:GOOGLE_APPLICATION_CREDENTIALS,
+    const auth = new JWT({
+        email: process.env.CLIENT_EMAIL,
+        key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
     
-    const client = await auth.getClient();
-    const googleSheets = google.sheets({version: 'v4', auth: client});
     //get metdata about spreadsheet
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId: spreadsheetId,
-    });
-
-    await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId: spreadsheetId,
-        range: 'גיליון1!A:B',
-        valueInputOption: 'USER_ENTERED',
-        resource: { 
-            values: [
-                [name,phone]
-            ]       
-        },
-    });
+    const doc = new GoogleSpreadsheet(spreadsheetId, auth);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    await sheet.addRow({Name:name,PhoneNumber:phone});
+    //read rows from spreadsheet
     res.send('success');
 });
 
