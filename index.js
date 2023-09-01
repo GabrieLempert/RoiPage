51f3dff22d69cb1730e557a1866ffdfab9665735
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2023-09-01 19:23:52 +0300

    final

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/',async (req, res) => {
     const sheet = doc.sheetsByIndex[0];
     await sheet.addRow({Name:name,PhoneNumber:phone});
     //read rows from spreadsheet
-    res.send('success');
+    res.redirect('/?message=Your request was sent successfully!');
 });
 
 
